0b8965d00d155c821817c48aee7275a5553dec08
Tom Dracz <tom.dracz@findsyou.com>
2015-04-09 16:02:04 +0100

    Scripts

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
-
+$(document).ready(function() {
+	$('.fade').hover(
+		function(){
+			$(this).find('.caption').fadeIn(250);
+		},
+		function(){
+			$(this).find('.caption').fadeOut(250);
+		}
+	);
+});
